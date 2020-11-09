c291d0c12ae54fdeef6d18494c7a044e90585cf2
guy <codingviewdmv@gmail.com>
2020-11-09 14:25:38 -0500

    ok

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -14,10 +14,10 @@ const initialState = {
 	montiel:montiel,
 	contact_response:false,
 	sessions:[
-	{age_range:'Ages 3-4',date_length:'Nov14 - Dec19',days_and_time:'Sat:9:00am-10:00am',requirements:'Includes 6 sessions with 2 coaches',price:'Cost $130.00',includes:{booli:true,text:'with T-Shirt included'}},
-	{age_range:'Ages 5-6',date_length:'Nov14 - Dec19',days_and_time:'Sat:10:00am-11:00am',requirements:'Includes 6 sessions with 2 coaches',price:'Cost $130.00',includes:{booli:true,text:'with T-shirts included'}},
+	{age_range:'Ages 3-4',date_length:'Nov14 - Dec19',days_and_time:'Sat:12:00pm-1:00pm',requirements:'Includes 6 sessions with 2 coaches',price:'Cost $130.00',includes:{booli:true,text:'with T-Shirt included'}},
+	{age_range:'Ages 5-6',date_length:'Nov14 - Dec19',days_and_time:'Sat:9:00am-10:00am',requirements:'Includes 6 sessions with 2 coaches',price:'Cost $130.00',includes:{booli:true,text:'with T-shirts included'}},
 	{age_range:'Ages 7-9',date_length:'Nov14 - Dec19',days_and_time:'Sat:10:00am-11:00am',requirements:'Includes 6 sessions with 2 coaches',price:'Cost $130.00',includes:{booli:true,text:'with T-shirts included'}},
-	{age_range:'Ages 10-13',date_length:'Nov14 - Dec19',days_and_time:'Sat:10:00am-11:00am',requirements:'Includes 6 sessions with 2 coaches',price:'Cost $130.00',includes:{booli:true,text:'with T-shirts included'}}
+	{age_range:'Ages 10-14',date_length:'Nov14 - Dec19',days_and_time:'Sat:11:00am-12:00pm',requirements:'Includes 6 sessions with 2 coaches',price:'Cost $130.00',includes:{booli:true,text:'with T-shirts included'}}
 
 	]
 
