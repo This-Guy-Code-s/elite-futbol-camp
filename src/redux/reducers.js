7ea1e70d3fc470845414f38bdd4cd6a33f0d2f26
guy <codingviewdmv@gmail.com>
2020-11-12 19:28:07 -0500

    updates on logo, requirements comp is new, time sessions, design bugs fixed

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,4 +1,4 @@
-import heroImage from '../util/imgs/blueBall.jpg'
+import heroImage from '../util/imgs/blueBall.png'
 import mapp from '../util/imgs/map1.png'
 import iceBall from '../util/imgs/sb1.png'
 import montiel from '../util/imgs/g.jpeg'
@@ -18,6 +18,13 @@ const initialState = {
 	{age_range:'Ages 5-6',date_length:'Nov14 - Dec19',days_and_time:'Sat:9:00am-10:00am',requirements:'Includes 6 sessions with 2 coaches',price:'Cost $130.00',includes:{booli:true,text:'with T-shirts included'}},
 	{age_range:'Ages 7-9',date_length:'Nov14 - Dec19',days_and_time:'Sat:10:00am-11:00am',requirements:'Includes 6 sessions with 2 coaches',price:'Cost $130.00',includes:{booli:true,text:'with T-shirts included'}},
 	{age_range:'Ages 10-14',date_length:'Nov14 - Dec19',days_and_time:'Sat:11:00am-12:00pm',requirements:'Includes 6 sessions with 2 coaches',price:'Cost $130.00',includes:{booli:true,text:'with T-shirts included'}}
+
+	],
+
+	reqs:[
+	{title:'No cleats',desc:'sneakers are ok but not the best for indoor'},
+	{title:'Outfit Type',desc:'wear athletic gear as shirts wont arrive before first session'},
+	// {title:'Covid',desc:'Parents must wear a mask at all times, Players wear a mask to get inside facility but can take off once training,Parents must sit at K sport parent area which they will be directed'},
 
 	]
 
